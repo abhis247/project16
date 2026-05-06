8780d02a9566c6ed195f286922b9d9afb9b8ef90
abhishekkumarcoder <abhishekkumar9801777@gmail.com>
2026-05-06 12:32:50 +0530

    configure vercel adapter and ignore css warnings

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter()
+  },
+
+  onwarn: (warning, handler) => {
+    if (warning.code === 'css-unused-selector') return;
+    handler(warning);
+  }
 };
 
 export default config;
